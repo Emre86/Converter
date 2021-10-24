b208befd8e7c86531bc412a9bca33947c7b285da
Emre86 <emre.gun86@gmail.com>
2021-10-24 21:54:57 +0200

    ajout des tests unitaires et code de conversion des chiffres romain en chiffre arabe

--- routes/converter.js
+++ routes/converter.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET converter listing. */
-router.get('/', function (req, res, next) {
-    return res.status(200).json({ "message": "OK" });
+function convertRomanToArabic(digits) {
+  let regex = new RegExp('^(M{0,4})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$');
+  let romanMap = new Map();
+  let result = 0;
+  romanMap.set("", 0)
+  romanMap.set("I", 1);
+  romanMap.set("II", 2);
+  romanMap.set("III", 3);
+  romanMap.set("IV", 4);
+  romanMap.set("V", 5);
+  romanMap.set("VI", 6);
+  romanMap.set("VII", 7);
+  romanMap.set("VIII", 8);
+  romanMap.set("IX", 9);
+  romanMap.set("X", 10);
+  romanMap.set("XX", 20);
+  romanMap.set("XXX", 30);
+  romanMap.set("XL", 40);
+  romanMap.set("L", 50);
+  romanMap.set("LX", 60);
+  romanMap.set("LXX", 70);
+  romanMap.set("LXXX", 80);
+  romanMap.set("XC", 90);
+  romanMap.set("C", 100);
+  romanMap.set("CC", 200);
+  romanMap.set("CCC", 300);
+  romanMap.set("CD", 400);
+  romanMap.set("D", 500);
+  romanMap.set("DC", 600);
+  romanMap.set("DCC", 700);
+  romanMap.set("DCCC", 800);
+  romanMap.set("CM", 900);
+  romanMap.set("M", 1000);
+  romanMap.set("MM", 2000);
+  romanMap.set("MMM", 3000);
+  romanMap.set("MMMM", 4000);
+  let romanDigits = digits.match(regex);
+  if (romanDigits) {
+    romanDigits.shift();
+    for (let rDigit of romanDigits) {
+      result += romanMap.get(rDigit);
+    }
+  }
+  return result;
+}
+
+
+/* GET converter */
+router.get('/:romanDigit', function (req, res, next) {
+  let arabicDigit = convertRomanToArabic(req.params.romanDigit);
+  return res.status(200).json({ "digit": arabicDigit });
 });
 
 module.exports = router;
--- spec/converter.spec.js
+++ spec/converter.spec.js
@@ -2,16 +2,257 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Server', () => {
-    describe('Converter', () => {
-        it('Test with JSON { "message" : "OK" }', (done) => {
-            request(app)
-                .get('/converter')
-                .expect(200)
-                .expect((response) => {
-                    expect(response.body.message).toEqual("OK");
-                })
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .end((error) => (error) ? done.fail(error) : done());
-        });
+  describe('Converter', () => {
+    it('Get /converter/I Response : { "digit" : 1 }', (done) => {
+      request(app)
+        .get('/converter/I')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(1);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
     });
+
+    it('Get /converter/II Response : { "digit" : 2 }', (done) => {
+      request(app)
+        .get('/converter/II')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(2);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+
+    it('Get /converter/III Response : { "digit" : 3 }', (done) => {
+      request(app)
+        .get('/converter/III')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(3);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/XXX Response : { "digit" : 30 }', (done) => {
+      request(app)
+        .get('/converter/XXX')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(30);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/CCC Response : { "digit" : 300 }', (done) => {
+      request(app)
+        .get('/converter/CCC')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(300);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/V Response : { "digit" : 5 }', (done) => {
+      request(app)
+        .get('/converter/V')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(5);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/L Response : { "digit" : 50 }', (done) => {
+      request(app)
+        .get('/converter/L')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(50);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/D Response : { "digit" : 500 }', (done) => {
+      request(app)
+        .get('/converter/D')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(500);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/LV Response : { "digit" : 55 }', (done) => {
+      request(app)
+        .get('/converter/LV')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(55);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+
+    it('Get /converter/DL Response : { "digit" : 550 }', (done) => {
+      request(app)
+        .get('/converter/DL')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(550);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/DLV Response : { "digit" : 555 }', (done) => {
+      request(app)
+        .get('/converter/DLV')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(555);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/VI Response : { "digit" : 6 }', (done) => {
+      request(app)
+        .get('/converter/VI')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(6);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/VIII Response : { "digit" : 8 }', (done) => {
+      request(app)
+        .get('/converter/VIII')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(8);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/XI Response : { "digit" : 11 }', (done) => {
+      request(app)
+        .get('/converter/XI')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(11);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/XIII Response : { "digit" : 13 }', (done) => {
+      request(app)
+        .get('/converter/XIII')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(13);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/LX Response : { "digit" : 60 }', (done) => {
+      request(app)
+        .get('/converter/LX')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(60);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/LXV Response : { "digit" : 65 }', (done) => {
+      request(app)
+        .get('/converter/LXV')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(65);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/IV Response : { "digit" : 4 }', (done) => {
+      request(app)
+        .get('/converter/IV')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(4);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/IX Response : { "digit" : 9 }', (done) => {
+      request(app)
+        .get('/converter/IX')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(9);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/XL Response : { "digit" : 40 }', (done) => {
+      request(app)
+        .get('/converter/XL')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(40);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/XC Response : { "digit" : 90 }', (done) => {
+      request(app)
+        .get('/converter/XC')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(90);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/CLIX Response : { "digit" : 159 }', (done) => {
+      request(app)
+        .get('/converter/CLIX')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(159);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/M Response : { "digit" : 1000 }', (done) => {
+      request(app)
+        .get('/converter/M')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(1000);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/MMMM Response : { "digit" : 4000 }', (done) => {
+      request(app)
+        .get('/converter/MMMM')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(4000);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+
+    it('Get /converter/MCCXXXVIII Response : { "digit" : 1238 }', (done) => {
+      request(app)
+        .get('/converter/MCCXXXVIII')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.digit).toEqual(1238);
+        })
+        .end((error) => (error) ? done.fail(error) : done());
+    });
+  });
 });
